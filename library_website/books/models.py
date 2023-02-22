c41b4b24756fb2c83468280742a1fbf04181fd75
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-02-21 23:20:31 -0800

    model update

--- library_website/books/models.py
+++ library_website/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class book(models.Model):
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self): #displays on admin panel
+        return self.title
