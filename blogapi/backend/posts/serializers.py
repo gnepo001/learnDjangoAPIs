d0000d697c2f3a1171147b80e093e5abaed967ae
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-03-02 03:01:43 -0800

    new update

---
+++ blogapi/backend/posts/serializers.py
@@ -0,0 +1,8 @@
+# posts/serializers.py
+from rest_framework import serializers 
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'author', 'title', 'body', 'created_at',) 
+        model = Post
