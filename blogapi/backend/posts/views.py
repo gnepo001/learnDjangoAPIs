da6019a5a2755e203327f5891a9e53461e7be2f7
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-03-04 21:53:28 -0800

    added api for only author of post can edit

--- blogapi/backend/posts/views.py
+++ blogapi/backend/posts/views.py
@@ -1,6 +1,7 @@
 # posts/views.py
 from rest_framework import generics , permissions
 from .models import Post
+from .permissions import IsAuthorOrReadOnly # new
 from .serializers import PostSerializer
 
 class PostList(generics.ListCreateAPIView):
@@ -9,6 +10,6 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView): 
-    permission_classes = (permissions.IsAuthenticated,) # new
+    permission_classes = (IsAuthorOrReadOnly,)  # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
