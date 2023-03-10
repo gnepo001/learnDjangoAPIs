2fa9e935d68569ba4da3dffbd302e04ba8b7cc03
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-03-09 19:21:08 -0800

    comment change

--- blogapi/backend/posts/views.py
+++ blogapi/backend/posts/views.py
@@ -5,11 +5,11 @@ from .permissions import IsAuthorOrReadOnly # new
 from .serializers import PostSerializer
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,) # new
+    permission_classes = (permissions.IsAuthenticated) # new
     queryset = Post.objects.all() 
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView): 
-    permission_classes = (IsAuthorOrReadOnly,)  # new
+    permission_classes = (IsAuthorOrReadOnly)  # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
